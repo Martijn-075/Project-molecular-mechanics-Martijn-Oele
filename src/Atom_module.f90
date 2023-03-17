0ff51126c4c7d08443648168bf7f29a361f70f87
Martijn <martijn.oele@hotmail.nl>
2023-03-17 20:06:50 +0100

    Added calculations for the angles of bonds

--- src/Atom_module.f90
+++ src/Atom_module.f90
@@ -1,10 +1,11 @@
 module atom_module
+use deg_rad
 implicit none
 
 integer, parameter :: realkind = 8
 
 private
-public atom, bond, bond_angle, molecule, read_atom, write_atom, print_atom, bonds_atom
+public atom, bond, bond_angle, molecule, read_atom, write_atom, print_atom, bonds_atom, angle_bonds
 
 type atom
 ! Setting the element in this project only C (carbon) or H (hydrogen)
@@ -20,15 +21,13 @@ integer :: link(2) = 0
 character(2) :: type = 'EE'
 ! Setting the calculated distance of the bond in A
 real(realkind) :: length = 0.
-! Setting the vector for the bond
-real(realkind) :: vector(3) = 0
 end type
 
 type bond_angle
 ! Setting the types of bonds over which the angle is calulated
 character(2) :: bonds(2) = (/'EE', 'EE'/)
 ! Setting the indicies of the atoms over wich the angle is calcualted
-integer :: atom_indicies(3)
+integer :: atom_indicies(3) = 0
 ! The actual angle of the bonds
 real(realkind) :: angle = 0.
 end type
@@ -37,6 +36,7 @@ type molecule
 type (atom), allocatable :: atoms(:)
 type (bond), allocatable :: bonds(:)
 type (bond_angle), allocatable :: angles(:)
+real(realkind), allocatable :: distance(:,:)
 end type
 
 
@@ -99,9 +99,18 @@ end subroutine print_atom
 subroutine bonds_atom(mol)
 type (molecule) :: mol
 integer :: i, j, k
-real(realkind) :: CC, CH, distance
+real(realkind) :: CC, CH
 
 allocate(mol%bonds(size(mol%atoms) - 1))
+
+allocate(mol%distance(size(mol%atoms), size(mol%atoms)))
+
+do i = 1, size(mol%atoms)
+    do j = 1,size(mol%atoms)
+        mol%distance(i,j) = sqrt((mol%atoms(i)%cords(1) - mol%atoms(j)%cords(1))**2 + &
+        (mol%atoms(i)%cords(2) - mol%atoms(j)%cords(2))**2 + (mol%atoms(i)%cords(3) - mol%atoms(j)%cords(3))**2)
+    enddo
+enddo
 
 CC = 1.535
 CH = 1.094
@@ -109,20 +118,16 @@ CH = 1.094
 k = 1
 do i = 1, size(mol%atoms) - 1
     do j = i + 1, size(mol%atoms)
-        distance = sqrt((mol%atoms(i)%cords(1) - mol%atoms(j)%cords(1))**2 + (mol%atoms(i)%cords(2) - mol%atoms(j)%cords(2))**2 &
-        + (mol%atoms(i)%cords(3) - mol%atoms(j)%cords(3))**2)
-        if (mol%atoms(i)%element == 'C' .and. mol%atoms(j)%element == 'C' .and. abs(CC - distance) < 0.1) then
+        if (mol%atoms(i)%element == 'C' .and. mol%atoms(j)%element == 'C' .and. abs(CC - mol%distance(i,j)) < 0.1) then
             mol%bonds(k)%link(1) = i
             mol%bonds(k)%link(2) = j
-            mol%bonds(k)%length = distance
-            mol%bonds(k)%vector = mol%atoms(i)%cords - mol%atoms(j)%cords
+            mol%bonds(k)%length = mol%distance(i,j)
             mol%bonds(k)%type = 'CC'
             k = k + 1
-        else if (mol%atoms(i)%element == 'C' .and. mol%atoms(j)%element == 'H' .and. abs(CH - distance) < 0.1) then
+        else if (mol%atoms(i)%element == 'C' .and. mol%atoms(j)%element == 'H' .and. abs(CH - mol%distance(i,j)) < 0.1) then
             mol%bonds(k)%link(1) = i
             mol%bonds(k)%link(2) = j
-            mol%bonds(k)%length = distance
-            mol%bonds(k)%vector = mol%atoms(i)%cords - mol%atoms(j)%cords
+            mol%bonds(k)%length = mol%distance(i,j)
             mol%bonds(k)%type = 'CH'
             k = k + 1
         end if
@@ -135,18 +140,71 @@ end subroutine bonds_atom
 
 subroutine angle_bonds(mol)
 type (molecule) :: mol
-integer :: n, i, k
+type (bond) :: bonds_holder(4,count(mol%atoms%element == 'C'))
+real(realkind) :: A, B, C
+integer :: carbon_indicies(count(mol%atoms%element == 'C'))
+integer :: n, i, j, k, l
 
 n = count(mol%atoms%element == 'C') * 6
 
 allocate(mol%angles(n))
 
 k = 1
+do j =1,size(mol%atoms)
+    if (mol%atoms(j)%element == 'C') then
+        carbon_indicies(k) = j
+        k = k + 1
+    end if
+enddo
 
 
+do i = 1, size(carbon_indicies)
+    k = 1
+    do j = 1,size(mol%bonds)
+        if (mol%bonds(j)%link(1) == carbon_indicies(i) .or. mol%bonds(j)%link(2) == carbon_indicies(i)) then
+            bonds_holder(k,i) = mol%bonds(j)
+            k = k + 1
+        end if
+    enddo
+end do
 
+
+k = 1
+do l = 1,size(bonds_holder, 2)
+    do i = 1,3
+        do j = i + 1,4
+            mol%angles(k)%bonds(1) = bonds_holder(i,l)%type
+            mol%angles(k)%bonds(2) = bonds_holder(j,l)%type
+
+            if (bonds_holder(i,l)%link(1) == bonds_holder(j,l)%link(1)) then
+                mol%angles(k)%atom_indicies(1) = bonds_holder(i,l)%link(2)
+                mol%angles(k)%atom_indicies(2) = bonds_holder(i,l)%link(1)
+                mol%angles(k)%atom_indicies(3) = bonds_holder(j,l)%link(2)
+            else if (bonds_holder(i,l)%link(1) == bonds_holder(j,l)%link(2)) then
+                mol%angles(k)%atom_indicies(1) = bonds_holder(i,l)%link(2)
+                mol%angles(k)%atom_indicies(2) = bonds_holder(i,l)%link(1)
+                mol%angles(k)%atom_indicies(3) = bonds_holder(j,l)%link(1)
+            else if (bonds_holder(i,l)%link(2) == bonds_holder(j,l)%link(1)) then
+                mol%angles(k)%atom_indicies(1) = bonds_holder(i,l)%link(1)
+                mol%angles(k)%atom_indicies(2) = bonds_holder(i,l)%link(2)
+                mol%angles(k)%atom_indicies(3) = bonds_holder(j,l)%link(2)
+            else
+                mol%angles(k)%atom_indicies(1) = bonds_holder(i,l)%link(1)
+                mol%angles(k)%atom_indicies(2) = bonds_holder(i,l)%link(2)
+                mol%angles(k)%atom_indicies(3) = bonds_holder(j,l)%link(1)
+           end if
+
+            A = mol%distance(mol%angles(k)%atom_indicies(1), mol%angles(k)%atom_indicies(2))
+            B = mol%distance(mol%angles(k)%atom_indicies(2), mol%angles(k)%atom_indicies(3))
+            C = mol%distance(mol%angles(k)%atom_indicies(1), mol%angles(k)%atom_indicies(3))
+            mol%angles(k)%angle =rad_to_deg(acos((A**2 + B**2 - C**2) / (2 * A * B)))
+            k = k + 1
+        enddo
+    enddo
+enddo
 
 end subroutine angle_bonds
+
 
 
 end module
